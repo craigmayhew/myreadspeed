58b18ea9e526ad5518190bd06b8782f22ec4aa6c
Craig Mayhew <craig@mayhew.io>
2018-01-31 15:58:05 +0000

    Removed share buttons

--- tools/createstatic.php
+++ tools/createstatic.php
@@ -133,21 +133,6 @@ class page{
                 <li><a href="/articles/">Learn to speed read</a></li>
                 <li><a href="/articles/10-tips/">10 tips to improve your reading speed</a></li>
             </ul>
-
-            <!-- AddThis Button BEGIN -->
-            <div class="addthis_toolbox addthis_default_style">
-            <a href="http://www.addthis.com/bookmark.php?v=250&pub=cnlifeasitis" class="addthis_button_compact">Share</a>
-            <span class="addthis_separator">|</span>
-            <a class="addthis_button_facebook"></a>
-            <a class="addthis_button_myspace"></a>
-            <a class="addthis_button_google"></a>
-            <a class="addthis_button_stumbleupon"></a>
-            <a class="addthis_button_digg"></a>
-            <a class="addthis_button_twitter"></a>
-            </div>
-            <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js?pub=cnlifeasitis"></script>
-            <!-- AddThis Button END -->
-
         </div>
         <div class="clear"></div>
     </div>
